9e100177c58e09e3ab60c93d563d9eb6ee75afe0
Elizabeth Fischer <eafischer2@alaska.edu>
2020-08-18 18:13:38 -0800

    Added machine file for Ubuntu 18.04LTS

---
+++ var/spack/environments/machines/u18.sh
@@ -0,0 +1,34 @@
+# Standard stuff for any loads-x environment on catalina
+
+# Enable Enviornment Modules (We're not using env modules)
+#source /Users/eafischer2/spack/opt/spack/darwin-catalina-x86_64/clang-11.0.3-apple/environment-modules-4.5.0-gxhm7oddr7rmyyc6zfud6atczhklk5gp/init/profile.sh
+
+
+# https://stackoverflow.com/questions/3430569/globbing-pathname-expansion-with-colon-as-separator
+function join() {
+    local IFS=$1
+    shift
+    echo "$*"
+}
+
+export HARNESS=$SPACK_ENV
+
+export SPACK_ENV_NAME=$(basename $SPACK_ENV)
+export SPACK_ROOT=$(dirname $(dirname $(dirname $(dirname $SPACK_ENV))))
+# This will be overwritten when the harness is created.
+
+# Minimal Spack setup without invoking Spack's setup_env.sh stuff
+export MODULEPATH=$MODULEPATH:$(join ':' $SPACK_ROOT/share/spack/modules/*)
+
+# Load the main environment
+#module purge
+
+# Load Spack-generated modules (or view-based env)
+PATH0="$PATH"
+source $SPACK_ENV/loads
+export PATH="$PATH:$PATH0"
+# This just messes things up
+#unset DYLD_LIBRARY_PATH
+unset LD_LIBRARY_PATH
+
+export PATH=$PATH:$SPACK_ROOT/bin
