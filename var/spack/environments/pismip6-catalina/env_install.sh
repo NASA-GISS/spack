86ce218f4161065228ddb1db3c78dc999e3fdc99
Elizabeth Fischer <elizabeth.fischer@alaska.edu>
2021-08-16 21:01:34 -0800

    Updates to pismip6-catalina environment

--- var/spack/environments/pismip6-catalina/env_install.sh
+++ var/spack/environments/pismip6-catalina/env_install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 #
 
+# NOTE: Run with python2 (see spack/bin/spack script if needed to prefer python2).  We need to avoid python 3.8
+
+# https://www.linuxquestions.org/questions/linux-newbie-8/configure-error-no-acceptable-ld-found-in-$path-102003/
+export LD=ld
 spack -e pismip6-catalina install
 
